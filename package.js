ae6a33969e7a31192131382466900b0860fed036
Patrick Lewis <patrickml.pml@gmail.com>
2016-04-15 13:01:17 -0400

    Added Update Funcitonality

--- package.js
+++ package.js
@@ -7,21 +7,24 @@ Package.describe({
   git: 'https://github.com/patrickml/singularity',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: 'README.md',
 });
 
-Package.onUse(function(api) {
+/* eslint-disable func-names, prefer-arrow-callback */
+Package.onUse(function (api) {
   api.versionsFrom('1.3');
   api.use([
     'ecmascript',
-    'meteorhacks:aggregate'
+    'meteorhacks:aggregate',
+    'patrickml:underscore-get-dot-to-not',
   ]);
   api.mainModule('entry.js');
 });
 
-Package.onTest(function(api) {
+Package.onTest(function (api) {
   api.use('ecmascript');
   api.use('tinytest');
   api.use('patrickml:singularity');
   api.mainModule('singularity-tests.js');
 });
+/* eslint-enable func-names, prefer-arrow-callback */

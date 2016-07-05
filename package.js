fed0f0bd51109f279bc56df09da5d0cf411d83e2
Patrick Lewis <patrickml.pml@gmail.com>
2016-07-05 14:10:13 -0400

    Updates

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'patrickml:singularity',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Object Document Mapper (ODM) for MeteorJS',
   // URL to the Git repository containing the source code for this package.

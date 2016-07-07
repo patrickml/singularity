5dc25804447276b23b3464b0210114720fdf2dbf
Patrick Lewis <patrickml.pml@gmail.com>
2016-07-07 12:06:35 -0400

    Removed Array support from deepmerge

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'patrickml:singularity',
-  version: '0.0.3',
+  version: '0.0.4',
   // Brief, one-line summary of the package.
   summary: 'Object Document Mapper (ODM) for MeteorJS',
   // URL to the Git repository containing the source code for this package.

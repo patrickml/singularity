d2dbbd89fa7032319e16c8e831e85dad7ba8004c
Patrick Lewis <patrickml.pml@gmail.com>
2016-07-29 09:52:49 -0400

    Fixed GeoNear

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'patrickml:singularity',
-  version: '0.0.7',
+  version: '0.0.8',
   // Brief, one-line summary of the package.
   summary: 'Object Document Mapper (ODM) for MeteorJS',
   // URL to the Git repository containing the source code for this package.
